3a521fd1ec48b3cb3ba47c702af3d85b8ce2cfa9
Charles Machalow <csm10495@gmail.com>
2019-01-30 19:06:16 -0800

    update setup/readme

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     author='csm10495',
     author_email='csm10495@gmail.com',
     url='http://github.com/csm10495/csmlog',
-    version='0.1a',
+    version='0.2a',
     packages=['csmlog'],
     license='MIT License',
     python_requires='>=2.7',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
